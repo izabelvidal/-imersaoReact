8fb10a848889f69d67d2c0fcd46fce9c2b2e7be4
Izabel Vidal <izabelvidal99@gmail.com>
2020-07-30 11:49:12 -0300

    final aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -53,7 +53,7 @@ function CadastroCategoria() {
 
 
         <FormField
-          label="Descrição:"
+          label="Descrição"
           type="????"
           name="descricao"
           value={values.descricao}
